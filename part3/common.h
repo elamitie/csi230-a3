552939e9952fe5ef0e79a6f96df4b11ae940a287
Ethan <elamitie@gmail.com>
2015-11-30 11:38:26 -0500

    clientserver

---
+++ part3/common.h
@@ -0,0 +1,13 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+
+#define DEFAULT_PORT "23032"
+#define DEFAULT_IP "127.0.0.1"
